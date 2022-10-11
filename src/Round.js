9517d30e911e851064af978e12eab8b9f039d5ef
Matt Walter <matt.walter.tx@outlook.com>
2022-10-11 17:23:39 -0500

    Feat: add method to return current card

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,18 @@
+const { createPromptModule } = require("inquirer");
+const Deck = require("./Deck");
+const Turn = require('./Turn');
+
 class Round {
-  constructor() {
-    
+  constructor(userDeck) {
+    this.gameDeck = userDeck.contents;
+    this.turnCount = 0;
+    this.currentCard = {};
+
+  }
+  returnCurrentCard() {
+    this.currentCard = this.gameDeck[this.turnCount];
+    // return this.gameDeck[this.turnCount]
+    return this.currentCard
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -4,24 +4,27 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const Round = require('../src/Round')
+const Round = require('../src/Round');
 
 describe('Round', function() {
-  let card, turn, deck, round;
+  let card1, card2, card3, turn1, turn2, deck, round;
 
   beforeEach(() => {
     card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     card2 = new Card(2, 'What is a comma-separated list of related values?', ["array", "object", "function"], 'array');
     card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ["mutator method", "accessor method", "iteration method"], 'mutator method');
-    turn1 = new Turn('object', card);
-    turn2 = new Turn('taters', card);
+    turn1 = new Turn('object', card1);
+    turn2 = new Turn('object', card2);
     deck = new Deck([card1, card2, card3]);
-    round = new Round('some stuff here')
+    round = new Round(deck);
   })
 
-  it('should be a round', function() {
-    console.log(turn1, card1)
-    expect(round).to.equal('some other stuff here')
+  it('should be able to return the current card', function() {
+    expect(round.returnCurrentCard()).to.equal(deck.contents[0]);
+    round.turnCount ++;
+    expect(round.returnCurrentCard()).to.equal(deck.contents[1]);
+    round.turnCount ++;
+    expect(round.returnCurrentCard()).to.equal(deck.contents[2]);
   })
 
 })
